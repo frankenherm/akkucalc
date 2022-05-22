e318cf8c165a0a12fdd9ead044bbb8e73a26d1eb
Rene Jennrich <Rene.Jennrich-Friederich@adesso.de>
2022-05-22 16:45:33 +0200

    result is working, refactoring in separate components

--- AkkuCalc/Services/NumberPartitioningService.cs
+++ AkkuCalc/Services/NumberPartitioningService.cs
@@ -7,17 +7,45 @@ namespace AkkuCalc.Services
 
         public ushort NumberOfSubsets { get; set; } = 1;
 
-        public IList<IEnumerable<float>> Subsets { get; set; } = new List<IEnumerable<float>>();
+        public IList<List<float>> Subsets { get; set; } = new List<List<float>>();
 
         public bool IsCalculated => Subsets.Any();
+
+        public async void Add(float number)
+        {
+            this.Numbers.Add(number);
+            this.OnNumberAdded();
+        }
+
+        public event EventHandler CalculatedChanged;
+
+        public void OnCalculatedChanged()
+        {
+            this.CalculatedChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler NumberAdded;
+
+        public void OnNumberAdded()
+        {
+            this.NumberAdded?.Invoke(this, EventArgs.Empty);
+        }
 
         public void DoPartition()
         {
             if (NumberOfSubsets < Numbers.Count())
             {
-                var partition = new effPartition.effPartition((List<float>)Numbers, NumberOfSubsets); ;
-                Subsets = partition.Subsets.Select(subset => subset.NumbIDs.Select(id => Numbers[(int)id])).ToList();
+                try
+                {
+                    var partition = new effPartition.effPartition((List<float>)Numbers, NumberOfSubsets);
+                    Subsets = partition.Subsets.Select(subset => subset.NumbIDs.Select(id => Numbers[(int)id-1]).ToList()).ToList();
+                }
+                catch (Exception e)
+                {
+                    //log
+                }
             }
+            this.OnCalculatedChanged();
         }
     }
 }
